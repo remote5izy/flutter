9674d483daff8a6d3c1c3e883287d520d2967995
remote5izy <103296316+remote5izy@users.noreply.github.com>
2024-04-10 20:05:54 +0800

    1

--- my_app2/lib/main.dart
+++ my_app2/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math'; // 導入 Random 類
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        fontFamily: 'Roboto', // 使用Roboto字體
+      ),
       home: CheckBoxPage(),
     );
   }
@@ -20,40 +23,78 @@ class CheckBoxPage extends StatefulWidget {
 }
 
 class _CheckBoxPageState extends State<CheckBoxPage> {
-  String? selectedActivity; // 用於保存選中的活動類型
-  List<String> allItems = ['研討會', '成果論壇', '新生說明會'];
+  String? selectedActivity;
+  List<String> selectedOptions = [];
+  Map<String, List<String>> activityOptions = {
+    '校慶': ['催場','表演','開場','介紹貴賓', '致詞', '大合照','頒獎','結束'],
+    '研討會及成果論壇': ['催場','開場','介紹貴賓', '致詞', '大合照','專題演講','中場休息','頒獎','結束'],
+    '新生說明會': ['催場', '表演','開場', '致詞', '大合照','中場休息','結束']
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CheckBox Demo'), // 標題欄標題
+        title: Text('司儀稿範例生成'),
       ),
-      body: ListView.builder(
-        itemCount: allItems.length,
-        itemBuilder: (context, index) {
-          return RadioListTile(
-            title: Text(allItems[index]), // 顯示每個選項的標題
-            value: allItems[index],
-            groupValue: selectedActivity,
-            onChanged: (value) {
-              setState(() {
-                selectedActivity = value as String?;
-              });
-            },
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            DropdownButton<String>(
+              isExpanded: true,
+              hint: Text('選擇活動類型'),
+              value: selectedActivity,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedActivity = value;
+                  selectedOptions.clear();
+                });
+              },
+              items: activityOptions.keys.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20), // 增加垂直間距
+            if (selectedActivity != null)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: activityOptions[selectedActivity!]!.length,
+                  itemBuilder: (context, index) {
+                    String option = activityOptions[selectedActivity!]![index];
+                    return CheckboxListTile(
+                      title: Text(option),
+                      value: selectedOptions.contains(option),
+                      onChanged: (bool? value) {
+                        setState(() {
+                          if (value ?? false) {
+                            selectedOptions.add(option);
+                          } else {
+                            selectedOptions.remove(option);
+                          }
+                        });
+                      },
+                    );
+                  },
+                ),
+              ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if (selectedActivity != null) {
-            // 只有在選中了一個活動類型時才能跳轉
+          if (selectedActivity != null && selectedOptions.isNotEmpty) {
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (context) => ArticlePage(
                   selectedActivity: selectedActivity!,
-                  articleContent: getArticleContent(selectedActivity!),
+                  selectedOptions: selectedOptions,
+                  articleContent: getArticleContent(selectedOptions),
                 ),
               ),
             );
@@ -61,7 +102,7 @@ class _CheckBoxPageState extends State<CheckBoxPage> {
             showDialog(
               context: context,
               builder: (context) => AlertDialog(
-                title: Text('請選擇一個活動類型'),
+                title: Text('請選擇活動類型和文章要呈現的內容'),
                 actions: [
                   TextButton(
                     onPressed: () {
@@ -74,86 +115,98 @@ class _CheckBoxPageState extends State<CheckBoxPage> {
             );
           }
         },
-        child: Icon(Icons.arrow_forward), // 浮動按鈕上的圖標
+        child: Icon(Icons.arrow_forward),
       ),
     );
   }
 
-  String getArticleContent(String selectedActivity) {
-    final Random random = Random();
-    String articleContent = "各位";
-
-    if (selectedActivity == '研討會') {
-      final randomIndex = random.nextInt(2); // 隨機生成 0 或 1
-      if (randomIndex == 0) {
-        articleContent += "貴賓/先進";
-      } else {
-        articleContent += "貴賓/學者";
+  List<String> getArticleContent(List<String> selectedOptions) {
+    List<String> articleContentList = [];
+    for (String option in selectedOptions) {
+      if (option == '催場') {
+        articleContentList.add("尊敬的各位貴賓，歡迎蒞臨「[活動名稱]」。請留意，我們即將於[時間]後正式開始活動。在活動開始之前，請您注意以下重要事項：首先，請於[催場時間]前入座，備妥自己的座位，以確保您不會錯過活動的任何部分。另外，本場地全面禁止飲食。如您需要飲食，請至會場外進行。同時，我們提供了洗手間設施，洗手間位於[洗手間位置]。為了保持活動的安靜和專注，請您將手機調為震動或靜音。此外，請勿錄製或攝影活動內容，這是為了尊重演講者和活動的版權。如果您需要紙本會議手冊，我們在報到處有提供有限量的手冊，請您前往領取。最後，如果您需要同步口譯服務，我們也在報到處提供了相關設備，歡迎您前往取用。感謝您的合作，讓我們共同為這場活動創造出一個順利而愉快的環境。謝謝大家。");
+      } else if (option == '表演') {
+        articleContentList.add("尊敬的各位貴賓，現在請您欣賞我們為大家精心準備的開場表演。今天，我們有幸邀請到了一個令人驚嘆的表演，由一群充滿熱情和才華的藝術家呈現。首先，我們將帶來一段動人心弦的音樂表演。請大家準備好感受音符的魔力，讓我們一起沉浸在美妙的音樂中。接下來，我們將欣賞一場令人目眩神迷的舞蹈表演。這將是一場充滿活力和靈感的舞蹈，帶給大家無限的驚喜和感動。讓我們用熱烈的掌聲歡迎我們的表演者，讓他們感受到我們的熱情和支持。現在，請讓我們將目光聚焦在舞台上，準備享受這場精彩絕倫的開場表演！讓音樂與舞蹈在這一刻開啟我們活動的序幕！");
+      } else if (option == '開場') {
+        articleContentList.add("尊敬的各位貴賓，歡迎蒞臨[活動名稱]，活動現在正式開始。");
+      } else if (option == '介紹貴賓') {
+        articleContentList.add("接下來介紹與會貴賓，首先歡迎OOO");
+      } else if (option == '致詞') {
+        articleContentList.add("貴賓致詞，首先用熱烈的掌聲歡迎OOO上台為我們致詞，謝謝OOO");
+      } else if (option == '大合照') {
+        articleContentList.add("接下來是大合照時間，讓我們攜手合影，共同留下這個難忘的時刻。3、2、1！非常感謝各位貴賓的配合，讓我們共同留下這個珍貴的瞬間。祝大家都能保持美好的笑容！");
+      } else if (option == '專題演講') {
+        articleContentList.add("首先第一場專題演講，讓我們有請OOO為我們擔任引言人，掌聲歡迎OOO");
+      } else if (option == '中場休息') {
+        articleContentList.add("上午[活動]圓滿告一段落，中午為貴賓準備餐盒，工作人員會招呼各位貴賓前往領餐，用餐地點在3F主會場與2樓用餐區，我們下午（一點）將會進行第五場專題演講。謝謝您的配合。");
+      } else if (option == '頒獎') {
+        articleContentList.add("現在，請頒獎人上台，為我們的得獎者頒獎。同時，請得獎者上台接受殊榮。");
+      } else if (option == '結束') {
+        articleContentList.add("感謝各位貴賓的熱情參與，今天的活動已經圓滿結束。讓我們一起為所有的演講者和得獎者送上最熱烈的掌聲！希望大家在這次活動中有所收穫，期待再次相聚。祝大家一路順風，再見！");
       }
-    } else if (selectedActivity == '成果論壇') {
-      articleContent += "貴賓/師長/同學";
-    } else if (selectedActivity == '新生說明會') {
-      articleContent += "師長/學弟妹";
     }
-
-    articleContent += "，大家好!";
-    return articleContent;
+    return articleContentList;
   }
 }
 
 class ArticlePage extends StatelessWidget {
   final String selectedActivity;
-  final String articleContent;
+  final List<String> selectedOptions;
+  final List<String> articleContent;
 
-  ArticlePage({required this.selectedActivity, required this.articleContent});
+  ArticlePage({
+    required this.selectedActivity,
+    required this.selectedOptions,
+    required this.articleContent,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('文章'), // 文章頁面標題
-        ),
-        body: Row(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(selectedActivity),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
           children: [
             Expanded(
-              child: ListTile(
-                title: Container(
-                  child: Text(selectedActivity), // 在这里显示选中的活动类型
-                ),
-                subtitle: Container(
-                  child: Text('9:00~10:00'),
-                ),
-              ),
-            ),
-            VerticalDivider(
-              color: Colors.black, // 垂直分隔线颜色
-              thickness: 2.0, // 垂直分隔线厚度
-              width: 0.0, // 垂直分隔线宽度
-            ),
-            Expanded(
-              flex: 3,
               child: ListView.separated(
+                itemCount: selectedOptions.length,
                 itemBuilder: (context, index) {
-                  return Container(
-                    child: ListTile(
-                      title: Container(
-                        child: Text('項目 ${index + 1}'),
-                      ),
-                      subtitle: Container(
-                        child: Text(articleContent), // 在这里显示文章内容
-                      ),
-                    ),
-                  );
+                  String option = selectedOptions[index];
+                  return Text(option);
                 },
                 separatorBuilder: (context, index) {
-                  // 在這裡建構分隔符
                   return Divider(
                     color: Colors.grey,
                     thickness: 1.0,
                   );
                 },
-                itemCount: 10, // 假設有10個項目
+              ),
+            ),
+            VerticalDivider(
+              color: Colors.black,
+              thickness: 2.0,
+            ),
+            Expanded(
+              flex: 3,
+              child: ListView.separated(
+                itemCount: articleContent.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    subtitle: Text(
+                      articleContent[index],
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: Colors.grey,
+                    thickness: 1.0,
+                  );
+                },
               ),
             ),
           ],
